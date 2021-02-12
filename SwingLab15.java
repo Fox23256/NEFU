494c930661d2c32ca44e0780b378e073980e437c
Daniil Davydov <clairdes.github@gmail.com>
2021-02-12 13:34:18 +0900

    Update SwingLab15.java

--- SwingLab15.java
+++ SwingLab15.java
@@ -1,14 +1,6 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SwingLab15 extends JFrame {
-    JLabel jlabel;
-    private JTextField txt;
-    final JButton but1;
-    final JButton but2;
-    private JButton but3;
-
     SwingLab15(){
         super("Приложение которое прибавляет к числу +1 и -1.");
         setSize(400,400);
@@ -17,44 +9,33 @@ public class SwingLab15 extends JFrame {
         a.setText("0");
         a.setBounds(100,50,50,20);
         a.setEditable(false);
-        jlabel= new JLabel("Number");
+        JLabel jlabel = new JLabel("Number");
         jlabel.setBounds(25,50,50,20);
-        but1 = new JButton("+1");
+        JButton but1 = new JButton("+1");
         but1.setBounds(200,50,50,20);
-        but2 = new JButton("-1");
+        JButton but2 = new JButton("-1");
         but2.setBounds(200,85,50,20);
         add(a);
         add(jlabel);
         add(but1);
-
         add(but2);
         setVisible(true);
-        but1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (Integer.parseInt(a.getText())<3)
+        but1.addActionListener(e -> {
+            if (Integer.parseInt(a.getText())<3)
                 a.setText(Integer.toString(Integer.parseInt(a.getText())+1));
-                else{
-                    System.out.println();
-                }
-
+            else{
+                System.out.println();
             }
         });
-        but2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (Integer.parseInt(a.getText())>-5)
+        but2.addActionListener(e -> {
+            if (Integer.parseInt(a.getText())>-5)
                 a.setText(Integer.toString(Integer.parseInt(a.getText())-1));
-                else{
-                    System.out.println();
-                }
+            else{
+                System.out.println();
             }
         });
-
-
-
     }
     public static void main(String[] args) {
-        SwingLab15 n = new SwingLab15();
+        new SwingLab15();
     }
 }
